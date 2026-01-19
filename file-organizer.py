44c0978a74c39843afbc2670ee8a41b5829d81a8
Kear Ibale <kdwest62@gmail.com>
2026-01-19 14:31:56 +0800

    Refactor directory creation and file organization logic

--- file-organizer.py
+++ file-organizer.py
@@ -1,17 +1,19 @@
 import os
 
-os.chdir('') # directory
-os.mkdir('Setups')
-os.mkdir('Images')
+os.chdir('')
+
+if not os.path.isdir('Setups'):
+    os.mkdir('Setups')
+if not os.path.isdir('Images'):
+    os.mkdir('Images')
 
 downloads = os.listdir(os.getcwd())
 for item in downloads:
     if os.path.isfile(item):
-        extension = "".join([item[-3], item[-2], item[-1]])
-        if extension == 'exe':
-            os.rename(item, '' + '\\' + item) # new directory
-        elif extension == 'jpg':
-            os.rename(item, '' + '\\' + item) # new directory
+        if item.lower().endswith('.exe'):
+            os.rename(item, '' + '\\' + item)
+        elif item.lower().endswith('.jpg'):
+            os.rename(item, '' + '\\' + item)
         else:
             continue
 print("File organization successful")
