d62bf8255f08db2c70e2bed4f491031dcb849696
Kear Ibale <kdwest62@gmail.com>
2026-01-21 14:18:55 +0800

    Refactor file organization to use shutil and count files
    Updated file organization logic to use shutil for moving files and added counters for .exe and .jpg files.

--- file-organizer.py
+++ file-organizer.py
@@ -1,19 +1,24 @@
 import os
+import shutil
 
-os.chdir('')
+os.chdir(r'')
 
 if not os.path.isdir('Setups'):
     os.mkdir('Setups')
 if not os.path.isdir('Images'):
     os.mkdir('Images')
 
+exe_count = 0
+jpg_count = 0
 downloads = os.listdir(os.getcwd())
 for item in downloads:
     if os.path.isfile(item):
         if item.lower().endswith('.exe'):
-            os.rename(item, '' + '\\' + item)
+            shutil.move(item, os.path.join(os.getcwd(), 'Setups'))
+            exe_count += 1
         elif item.lower().endswith('.jpg'):
-            os.rename(item, '' + '\\' + item)
+            shutil.move(item, os.path.join(os.getcwd(), 'Images'))
+            jpg_count += 1
         else:
             continue
-print("File organization successful")
+print(f"Success! Setups: {exe_count} files, Images: {jpg_count} files")
