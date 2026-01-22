c19408a7b4870dcd0cb27ba32a9281bdb0e67d8e
Kear Ibale <kdwest62@gmail.com>
2026-01-22 14:26:10 +0800

    Add dynamic folder creation for file organization

--- file-organizer.py
+++ file-organizer.py
@@ -4,10 +4,12 @@ import shutil
 path = input("Path: ")
 os.chdir(path)
 
-if not os.path.isdir('Setups'):
-    os.mkdir('Setups')
-if not os.path.isdir('Images'):
-    os.mkdir('Images')
+folder1 = input("Folder 1: ")
+if not os.path.isdir(folder1):
+    os.mkdir(folder1)
+folder2 = input("Folder 2: ")
+if not os.path.isdir(folder2):
+    os.mkdir(folder2)
 
 exe_count = 0
 jpg_count = 0
@@ -15,11 +17,11 @@ downloads = os.listdir(os.getcwd())
 for item in downloads:
     if os.path.isfile(item):
         if item.lower().endswith('.exe'):
-            shutil.move(item, os.path.join(os.getcwd(), 'Setups'))
+            shutil.move(item, os.path.join(os.getcwd(), folder1))
             exe_count += 1
         elif item.lower().endswith('.jpg'):
-            shutil.move(item, os.path.join(os.getcwd(), 'Images'))
+            shutil.move(item, os.path.join(os.getcwd(), folder2))
             jpg_count += 1
         else:
             continue
-print(f"Success! Setups: {exe_count} files, Images: {jpg_count} files")
+print(f"Success! {folder1}: {exe_count} files, {folder2}: {jpg_count} files")
