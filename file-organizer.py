fae57670108ebd0518f5d1dfa3d7d9339c7f0378
Kear Ibale <kdwest62@gmail.com>
2026-01-26 15:03:13 +0800

    Implement file organization by extension
    Added functionality to organize files by extension into user-defined directories.

--- file-organizer.py
+++ file-organizer.py
@@ -1,27 +1,60 @@
 import os
 import shutil
 
+def file_extension(filename):
+    filename_split = filename.split('.')
+    return filename_split[-1]
+
+# input desired path
 path = input("Path: ")
 os.chdir(path)
 
-folder1 = input("Folder 1: ")
-if not os.path.isdir(folder1):
-    os.mkdir(folder1)
-folder2 = input("Folder 2: ")
-if not os.path.isdir(folder2):
-    os.mkdir(folder2)
+# input number of directories to create + their names
+dirs_list = []
+total_dirs = int(input("Number of directories to create: "))
+dir_count = 1
+while dir_count <= total_dirs:
+    dir_name= input(f"Directory {dir_count} name: ")
+    os.mkdir(dir_name)
+    dirs_list.append(dir_name)
+    dir_count += 1
 
-exe_count = 0
-jpg_count = 0
-downloads = os.listdir(os.getcwd())
-for item in downloads:
+# program prints file extension/s located in path
+draft_list = []
+items = os.listdir(os.getcwd())
+for item in items:
     if os.path.isfile(item):
-        if item.lower().endswith('.exe'):
-            shutil.move(item, os.path.join(os.getcwd(), folder1))
-            exe_count += 1
-        elif item.lower().endswith('.jpg'):
-            shutil.move(item, os.path.join(os.getcwd(), folder2))
-            jpg_count += 1
-        else:
-            continue
-print(f"Success! {folder1}: {exe_count} files, {folder2}: {jpg_count} files")
+        draft_list.append(file_extension(item))
+file_extensions = set(draft_list)
+
+# user chooses the file extension/s to organize
+extensions_list = []
+print(f"File extensions inside the path: {file_extensions}")
+total_ext = int(input("Number of file extensions to organize: "))
+print("Names:")
+ext_count = 1
+while ext_count <= total_ext:
+    extension = input(f"File extension {ext_count}: ")
+    if extension not in file_extensions:
+        print("File extension not found. Try again")
+    else:
+        extensions_list.append(extension)
+        ext_count += 1
+
+# user chooses which directories the file extension/s go
+destinations_list = []
+print(f"Directories created: {dirs_list}")
+for item in extensions_list:
+    destination = input(f"Directory for {item}: ")
+    destinations_list.append(destination)
+blueprint = dict(zip(extensions_list, destinations_list))
+
+# main process
+items = os.listdir(os.getcwd())
+for item in items:
+    if os.path.isfile(item):
+        if file_extension(item) in blueprint.keys():
+            shutil.move(item, os.path.join(os.getcwd(), blueprint[file_extension(item)]))
+
+# feedback
+print("File organization successful")
