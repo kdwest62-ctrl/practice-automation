efb018b14417fb7cee645fe3d3c880fda378cef8
Kear Ibale <kdwest62@gmail.com>
2026-01-29 16:30:14 +0800

    Improve duplicate file finder functionality
    Refactor file listing logic and add duplicate deletion feature.

--- duplicate-file-finder.py
+++ duplicate-file-finder.py
@@ -3,15 +3,15 @@ import os
 path1_files = []
 path1 = input("Path 1: ")
 os.chdir(path1)
-path1 = os.listdir(os.getcwd())
-for item in path1:
+path1_items = os.listdir(os.getcwd())
+for item in path1_items:
     if os.path.isfile(item):
         path1_files.append(item)
 path2_files = []
 path2 = input("Path 2: ")
 os.chdir(path2)
-path2 = os.listdir(os.getcwd())
-for item in path2:
+path2_items = os.listdir(os.getcwd())
+for item in path2_items:
     if os.path.isfile(item):
         path2_files.append(item)
 
@@ -27,3 +27,16 @@ if len(duplicates) == 0:
     print("No duplicate files")
 else:
     print(f"Duplicate files: {set(duplicates)}")
+    choice = input("Delete duplicates? (y/n): ")
+    if choice == 'y':
+        path = input("Select path: ")
+        if path == '1':
+            filename = input("Filename: ")
+            os.remove(os.path.join(path1, filename))
+            print("Success! Duplicates deleted")
+        elif path == '2':
+            filename = input("Filename: ")
+            os.remove(os.path.join(path2, filename))
+            print("Success! Duplicates deleted")
+        else:
+            print("Path doesn't exist")
