2d69c6d391e69be169e559cbba82f92efb474d7f
Kear Ibale <kdwest62@gmail.com>
2026-01-31 08:44:19 +0800

    Refactor file backup script with error handling

--- file-backup.py
+++ file-backup.py
@@ -1,21 +1,23 @@
 import os
 import shutil
 
-dst = input("Destination path: ")
-os.chdir(dst)
-dir_name = input("Directory name: ")
-dst_dir = os.path.join(dst, dir_name)
-if not os.path.isdir(dst_dir):
+try:
+    dst = input("Destination path: ")
+    os.chdir(dst)
+    dir_name = input("Directory name: ")
+    dst_dir = os.path.join(dst, dir_name)
     os.mkdir(dst_dir)
 
-src = input("Source path: ")
-os.chdir(src)
-extension = input("File extension: ")
-count = 0
-items = os.listdir(os.getcwd())
-for item in items:
-    if os.path.isfile(item):
-        if item.endswith(extension):
-            shutil.copy(item, dst_dir)
-            count += 1
-print(f"{count} files copied")
+    src = input("Source path: ")
+    os.chdir(src)
+    extension = input("File extension: ")
+    count = 0
+    items = os.listdir(os.getcwd())
+    for item in items:
+        if os.path.isfile(item):
+            if item.endswith(extension):
+                shutil.copy(item, dst_dir)
+                count += 1
+    print(f"{count} files copied")
+except FileExistsError:
+    print("Directory already exists")
