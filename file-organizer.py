fff81b74143325499539d40322181de509a0a5b7
Kear Ibale <kdwest62@gmail.com>
2026-02-02 19:37:33 +0800

    Refactor directory creation and file organization logic

--- file-organizer.py
+++ file-organizer.py
@@ -11,49 +11,51 @@ try:
 
     dirs_list = []
     total_dirs = int(input("Number of directories to create: "))
-    dir_count = 1
-    while dir_count <= total_dirs:
-        dir_name= input(f"Directory {dir_count} name: ")
-        os.mkdir(dir_name)
-        dirs_list.append(dir_name)
-        dir_count += 1
+    if total_dirs != 0:
+        dir_count = 1
+        while dir_count <= total_dirs:
+            dir_name= input(f"Directory {dir_count} name: ")
+            os.mkdir(dir_name)
+            dirs_list.append(dir_name)
+            dir_count += 1
 
-    draft_list = []
-    items = os.listdir(os.getcwd())
-    for item in items:
-        if os.path.isfile(item):
-            draft_list.append(file_extension(item))
-    file_extensions = set(draft_list)
-    print(file_extensions)
-
-    extensions_list = []
-    total_ext = int(input("Number of file extensions to organize: "))
-    ext_count = 1
-    while ext_count <= total_ext:
-        extension = input(f"File extension {ext_count} name: ")
-        if extension not in file_extensions:
-            print("File extension not found. Try again")
-        else:
-            extensions_list.append(extension)
-            ext_count += 1
-
-    destinations_list = []
-    print(f"Directories: {dirs_list}")
-    for item in extensions_list:
-        destination = input(f"Directory for {item}: ")
-        if destination not in dirs_list:
-            print("Directory not available")
-        else:
-            destinations_list.append(destination)
-    organizer = dict(zip(extensions_list, destinations_list))
-
-    if len(organizer.values()) != 0:
+        draft_list = []
         items = os.listdir(os.getcwd())
         for item in items:
             if os.path.isfile(item):
-                if file_extension(item) in organizer.keys():
-                    shutil.move(item, os.path.join(os.getcwd(), organizer[file_extension(item)]))
-        print("File organization successful")
+                draft_list.append(file_extension(item))
+        file_extensions = set(draft_list)
+        print(file_extensions)
+
+        extensions_list = []
+        total_ext = int(input("Number of file extensions to organize: "))
+        if total_ext != 0:
+            ext_count = 1
+            while ext_count <= total_ext:
+                extension = input(f"File extension {ext_count} name: ")
+                if extension not in file_extensions:
+                    print("File extension not found. Try again")
+                else:
+                    extensions_list.append(extension)
+                    ext_count += 1
+
+            destinations_list = []
+            print(f"Directories: {dirs_list}")
+            for item in extensions_list:
+                destination = input(f"Directory for {item}: ")
+                if destination not in dirs_list:
+                    print("Directory not available")
+                else:
+                    destinations_list.append(destination)
+            organizer = dict(zip(extensions_list, destinations_list))
+
+            if len(organizer.values()) != 0:
+                items = os.listdir(os.getcwd())
+                for item in items:
+                    if os.path.isfile(item):
+                        if file_extension(item) in organizer.keys():
+                            shutil.move(item, os.path.join(os.getcwd(), organizer[file_extension(item)]))
+                print("File organization successful")
 except FileExistsError:
     print("Directory already exists")
 except FileNotFoundError:
