6ac484c75c04a16a307b766bb7f065d24642a221
Kear Ibale <kdwest62@gmail.com>
2026-02-03 15:13:07 +0800

    Add file rename count tracking
    Track the number of renamed files and print the count.

--- file-renamer.py
+++ file-renamer.py
@@ -5,9 +5,11 @@ os.chdir(dir_path)
 
 files = os.listdir(os.getcwd())
 num = 1
+renamed = 0
 for file in files:
     if os.path.isfile(file):
         if file.endswith('png'):
             os.rename(file, f'DES{num}.png')
             num += 1
-print("Files successfully renamed")
+            renamed += 1
+print(f"{renamed} files successfully renamed")
