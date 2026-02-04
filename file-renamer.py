668b68e09e2a488ba7c921e9195653dae4bff612
Kear Ibale <kdwest62@gmail.com>
2026-02-04 14:35:55 +0800

    Implement error handling and refactor renaming logic
    Added error handling for invalid directory paths and refactored the renaming logic to be inside a try-except block.

--- file-renamer.py
+++ file-renamer.py
@@ -1,15 +1,18 @@
 import os
 
-dir_path = input("Directory path: ")
-os.chdir(dir_path)
+try:
+    dir_path = input("Directory path: ")
+    os.chdir(dir_path)
 
-files = os.listdir(os.getcwd())
-num = 1
-renamed = 0
-for file in files:
-    if os.path.isfile(file):
-        if file.endswith('png'):
-            os.rename(file, f'DES{num}.png')
-            num += 1
-            renamed += 1
-print(f"{renamed} files successfully renamed")
+    files = os.listdir(os.getcwd())
+    num = 1
+    renamed = 0
+    for file in files:
+        if os.path.isfile(file):
+            if file.endswith('png'):
+                os.rename(file, f'DES{num}.png')
+                num += 1
+                renamed += 1
+    print(f"{renamed} files successfully renamed")
+except FileNotFoundError:
+    print("Path not found")
