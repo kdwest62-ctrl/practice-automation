d037651952287288587b0f6072b8d0acaecdc2fb
Kear Ibale <kdwest62@gmail.com>
2026-02-05 11:30:51 +0800

    Refactor duplicate file finder functionality
    Refactor duplicate file finder to use a single path input and simplify duplicate detection logic.

--- duplicate-file-finder.py
+++ duplicate-file-finder.py
@@ -1,35 +1,28 @@
 import os
+from pathlib import Path
 
-try:
-    path1 = input("Path 1: ")
-    path1_items = os.listdir(path1)
-    path2 = input("Path 2: ")
-    path2_items = os.listdir(path2)
+source = input("Path: ")
+path = Path(source)
 
-    duplicates = []
-    for item in path1_items:
-        if item in path2_items:
-            duplicates.append(item)
-    for item in path2_items:
-        if item in path1_items:
-            duplicates.append(item)
+files = []
+list2 = []
+duplicates = []
 
-    if len(duplicates) == 0:
-        print("No duplicate files")
+for _, dirs, filename in os.walk(path):
+    for item in dirs:
+        if os.path.isfile(item):
+            files.append(item)
+    for item in filename:
+        files.append(item)
+
+for item in files:
+    if item not in list2:
+        list2.append(item)
     else:
-        print(f"Duplicate files: {set(duplicates)}")
-        choice = input("Delete duplicates? (y/n): ")
-        if choice == 'y':
-            path = input("Select path: ")
-            if path == '1':
-                filename = input("Filename: ")
-                os.remove(os.path.join(path1, filename))
-                print("Success! Duplicates deleted")
-            elif path == '2':
-                filename = input("Filename: ")
-                os.remove(os.path.join(path2, filename))
-                print("Success! Duplicates deleted")
-            else:
-                print("Path doesn't exist")
-except FileNotFoundError:
-    print("Filename not found")
+        duplicates.append(item)
+
+if len(duplicates) == 0:
+    print("No duplicates")
+else:
+    for item in duplicates:
+        print(item)
