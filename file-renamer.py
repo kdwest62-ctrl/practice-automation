7606012e464fe8d43f0d23440f8e6f5e94430368
Kear Ibale <kdwest62@gmail.com>
2026-02-20 20:11:08 +0800

    Refactor file renaming to use pathlib

--- file-renamer.py
+++ file-renamer.py
@@ -1,17 +1,17 @@
-import os
+from pathlib import Path
 
-dir_path = input("Directory path: ")
-if os.path.exists(dir_path):
-    files = os.listdir(dir_path)
+dir_path = Path(input("Directory path: "))
+if dir_path.exists():
+    files = dir_path.iterdir()
     num = 1
     renamed = 0
     for file in files:
-        file_path = os.path.join(dir_path, file)
-        if os.path.isfile(file_path):
-            if file.endswith('png'):
-                os.rename(file_path, os.path.join(dir_path, f'DES{num}.png'))
+        if file.is_file():
+            if file.suffix == '.png':
+                new_name = file.parent / f'DES{num}.png'
+                file.rename(new_name)
                 num += 1
                 renamed += 1
     print(f"{renamed} files renamed")
 else:
-    print("Path not found")
+   print("Path not found")
