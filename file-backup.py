804f674d00a8422311c2296e6df492b57e10738f
Kear Ibale <kdwest62@gmail.com>
2026-02-23 18:38:17 +0800

    Refactor file backup script with pathlib
    Refactor file backup script to use pathlib for path handling and improve directory creation logic.

--- file-backup.py
+++ file-backup.py
@@ -1,18 +1,14 @@
-import os
 import shutil
 from pathlib import Path
 
 try:
-    src_path = input("Source path: ")
-    if os.path.exists(src_path):
-
+    src_path = Path(input("Source path: "))
+    if src_path.exists():
         file_extensions = []
-        items = os.listdir(src_path)
+        items = src_path.iterdir()
         for item in items:
-            file_path = os.path.join(src_path, item)
-            if os.path.isfile(file_path):
-                x = Path(file_path)
-                file_extensions.append(x.suffix)
+            if item.is_file():
+                file_extensions.append(item.suffix)
         print(set(file_extensions))
 
         extensions_list = []
@@ -21,67 +17,74 @@ try:
             ext_count = 1
             while ext_count <= total_ext:
                 extension = input(f"File extension {ext_count} name: ")
-                if extension not in file_extensions:
-                    print("File extension not found. Try again")
-                else:
+                if extension in file_extensions:
                     extensions_list.append(extension)
                     ext_count += 1
-
-        dst_path = input("Destination path: ")
-
-        dirs_list = []
-        total_dirs = int(input("Number of directories to create: "))
-        if total_dirs != 0:
-            dir_count = 1
-            while dir_count <= total_dirs:
-                dir_name= input(f"Directory {dir_count} name: ")
-                os.mkdir(os.path.join(dst_path, dir_name))
-                dirs_list.append(dir_name)
-                dir_count += 1
-
-            destinations_list = []
-            print(dirs_list)
-            for item in extensions_list:
-                destination = input(f"Directory for {item}: ")
-                if destination not in dirs_list:
-                    print("Directory not available")
                 else:
-                    destinations_list.append(destination)
-            organizer = dict(zip(extensions_list, destinations_list))
+                    print("File extension not found. Try again")
 
-            if len(organizer.values()) != 0:
-                items = os.listdir(src_path)
-                for item in items:
-                    file_path = os.path.join(src_path, item)
-                    if os.path.isfile(file_path):
-                        a = Path(file_path)
-                        if a.suffix in organizer.keys():
-                            shutil.copy2(file_path, os.path.join(dst_path, organizer[a.suffix]))
+        dst_path = Path(input("Destination path: "))
+        if dst_path.exists():
+            dirs_list = []
+            total_dirs = int(input("Number of directories to create: "))
+            if total_dirs != 0:
+                dir_count = 1
+                while dir_count <= total_dirs:
+                    dir_name= input(f"Directory {dir_count} name: ")
+                    new_dir_path = dst_path / dir_name
+                    new_dir_path.mkdir()
+                    dirs_list.append(dir_name)
+                    dir_count += 1
 
-                dirs_files = []
-                for item in dirs_list:
-                    length = len(os.listdir(os.path.join(dst_path, item)))
-                    dirs_files.append(length)
-                files_count = dict(zip(dirs_list, dirs_files))
-                for key, value in files_count.items():
-                    print(f"Files copied to {key}: {value}")
-
-                compress = input("Compress directory? (y/n): ")
-                if compress == 'y':
-                    print(dirs_list)
-                    name = input("Directory name: ")
-                    if name in dirs_list:
-                        archive_name = input("Archive name: ")
-                        archive = os.path.join(dst_path, archive_name)
-                        shutil.make_archive(archive, 'zip', os.path.join(dst_path, name))
-                        print(f"Success! Directory {name} compressed")
+                destinations_list = []
+                print(dirs_list)
+                for item in extensions_list:
+                    destination = input(f"Directory for {item}: ")
+                    if destination in dirs_list:
+                        destinations_list.append(destination)
                     else:
-                        print("Directory not found")
+                        print("Directory not available")
+                organizer = dict(zip(extensions_list, destinations_list))
+
+                if len(organizer.values()) != 0:
+                    items = src_path.iterdir()
+                    for item in items:
+                        if item.is_file():
+                            if item.suffix in organizer.keys():
+                                a = str(item)
+                                b = str(dst_path / organizer[item.suffix])
+                                shutil.copy2(a, b)
+
+                    dirs_files = []
+                    c = []
+                    for item in dirs_list:
+                        new_path = dst_path / item
+                        files = new_path.iterdir()
+                        for y in files:
+                            c.append(y)
+                        length = len(c)
+                        dirs_files.append(length)
+                    files_count = dict(zip(dirs_list, dirs_files))
+                    for key, value in files_count.items():
+                        print(f"Files copied to {key}: {value}")
+
+                    compress = input("Compress directory? (y/n): ")
+                    if compress == 'y':
+                        print(dirs_list)
+                        name = input("Directory name: ")
+                        if name in dirs_list:
+                            archive_name = input("Archive name: ")
+                            archive = dst_path / archive_name
+                            compress_path = dst_path / name
+                            shutil.make_archive(archive, 'zip', compress_path)
+                            print(f"Success! Directory {name} compressed")
+                        else:
+                            print("Directory not found")
+        else:
+            print("Path not found")
     else:
         print("Path not found")
 except FileExistsError:
     print("Directory already exists")
-except FileNotFoundError:
-    print("Path not found")
 except ValueError:
     print("Please input a number")
