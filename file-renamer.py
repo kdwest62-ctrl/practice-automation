54f03bd0c91499b63c4178cd046e0cca7cd86fe2
Kear Ibale <kdwest62@gmail.com>
2026-02-24 14:48:35 +0800

    Enhance file renamer to support extension-based renaming
    Updated the file renamer to allow renaming by user-specified file extension and list available extensions.

--- file-renamer.py
+++ file-renamer.py
@@ -2,16 +2,27 @@ from pathlib import Path
 
 dir_path = Path(input("Directory path: "))
 if dir_path.exists():
-    files = dir_path.iterdir()
-    num = 1
-    renamed = 0
-    for file in files:
-        if file.is_file():
-            if file.suffix == '.png':
-                new_name = file.parent / f'DES{num}.png'
-                file.rename(new_name)
-                num += 1
-                renamed += 1
-    print(f"{renamed} files renamed")
+    file_extensions = []
+    items = dir_path.iterdir()
+    for item in items:
+        if item.is_file():
+            file_extensions.append(item.suffix)
+    print(set(file_extensions))
+
+    file_extension = input("File extension to rename: ")
+    if file_extension in file_extensions:
+        num = 1
+        renamed = 0
+        items = dir_path.iterdir()
+        for item in items:
+            if item.is_file():
+                if item.suffix == file_extension:
+                    new_name = item.parent / f'test{num}{file_extension}'
+                    item.rename(new_name)
+                    num += 1
+                    renamed += 1
+        print(f"{renamed} files renamed")
+    else:
+        print("File extension not available")
 else:
-   print("Path not found")
+    print("Path not found")
