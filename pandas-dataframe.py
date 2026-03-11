10596de3daae87e5ff8cbc03143580448a66e0b0
Kear Ibale <kdwest62@gmail.com>
2026-03-12 07:43:32 +0800

    Update DataFrame for starters and add bench players

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
-celtics = {
-    'Player': ['Brown', 'Tatum', 'Pritchard', 'White', 'Hauser'],
-    'Position': ['FW', 'FW', 'PG', 'G', 'FW']
+starters = {
+    'Player': ['White', 'Brown', 'Hauser', 'Tatum', 'Queta'],
+    'Position': ['PG', 'SG', 'SF', 'PF', 'C']
 }
-df = pd.DataFrame(celtics, index=['7', '11', '0', '9', '30'])
+df = pd.DataFrame(starters, index=['9', '7', '30', '0', '88'])
 print(df)
+
+bench = {
+    'Player': ['Pritchard', 'Scheierman', 'Walsh', 'Gonzalez', 'Garza', 'Vucevic'],
+    'Position': ['G', 'G', 'G', 'G', 'C', 'C']
+}
+df_bench = pd.DataFrame(bench, index=['11', '55', '27', '28', '52', '4'])
+two_way = pd.DataFrame([{'Player': 'Harper Jr.', 'Position': 'G/F'},
+                         {'Player': 'Shulga', 'Position': 'G'},
+                         {'Player': 'Tonje', 'Position': 'G'},
+                         {'Player': 'Williams','Position': 'F/C'}], index=['13', '44', '8', '77'])
+df_bench = pd.concat([df_bench, two_way])
+print(df_bench)
