e572cc2e79ab5686f89f36521214549d13ef6a13
Kear Ibale <kdwest62@gmail.com>
2026-03-11 08:26:45 +0800

    Replace static drivers list with user input

--- pandas-series.py
+++ pandas-series.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-drivers = ['Russell', 'Antonelli', 'Leclerc']
+p1 = input("First: ")
+p2 = input("Second: ")
+p3 = input("Third: ")
+drivers = [p1, p2, p3]
 podium = pd.Series(drivers, index=['P1', 'P2', 'P3'])
 print(podium)
