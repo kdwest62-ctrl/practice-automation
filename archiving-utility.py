e99852c3a17f84ef0519a5db9cb6bfd113333128
Kear Ibale <kdwest62@gmail.com>
2026-04-04 19:50:21 +0800

    Enhance archiving utility for multiple directories
    Updated archiving process to allow multiple directories to be archived with individual names.

--- archiving-utility.py
+++ archiving-utility.py
@@ -11,16 +11,29 @@ if path.exists():
     if len(dirs) > 0:
         for item in dirs:
             print(item.name)
-        dir_to_archive = input("Directory to archive: ")
-        archive_name = input("Archive name: ")
+
+        dirs_to_archive = []
+        total = int(input("Number of directories to archive: "))
+        for num in range(total):
+            dir_to_archive = input(f"Directory {num + 1} to archive: ")
+            dirs_to_archive.append(dir_to_archive)
+
+        archive_names = []
+        for item in dirs_to_archive:
+            archive_name = input(f"Archive name for {item}: ")
+            archive_names.append(archive_name)
+
+        my_dict = dict(zip(dirs_to_archive, archive_names))
+
         dst_path = Path(input("Destination path: "))
         if dst_path.exists():
-            src = path / dir_to_archive
-            dst = dst_path / archive_name
-            shutil.make_archive(dst, 'zip', src)
-            print(f"{dir_to_archive} archived as {archive_name}")
+            for key, value in my_dict.items():
+                src = path / key
+                dst = dst_path / value
+                shutil.make_archive(dst, 'zip', src)
+                print(f"{key} archived as {value}")
         else:
-            print("Path not found")
+            print("Destination path not found")
     else:
         print("No directories in path")
 else:
