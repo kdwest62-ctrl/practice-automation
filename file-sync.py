051729e89c414a8957e199c2bb0bf5f7a4facc8d
Kear Ibale <kdwest62@gmail.com>
2026-04-12 07:42:39 +0800

    Implement file check and optimize sync process
    Added a check function to compare source files with directory files and modified sync logic to avoid unnecessary syncing.

--- file-sync.py
+++ file-sync.py
@@ -9,6 +9,11 @@ if source.exists():
         for file in src_files:
             if file not in dir_files:
                 shutil.copy2(file, dir_path)
+    def check(src_files, dir_files):
+        if dir_files == src_files:
+            return 0
+        else:
+            return 1
 
     source_files = get_files(source)
     if len(source_files) > 0:
@@ -27,12 +32,18 @@ if source.exists():
             files_of_paths = []
             for item in paths:
                 files_of_paths.append(get_files(item))
+            scores = []
+            for item in files_of_paths:
+                scores.append(check(source_files, item))
 
-            to_sync = dict(zip(paths, files_of_paths))
-            for path, files in to_sync.items():
-                if files != source_files:
-                    sync(source_files, path, files)
-            print("Success! Directories synced")
+            if sum(scores) != 0:
+                to_sync = dict(zip(paths, files_of_paths))
+                for path, files in to_sync.items():
+                    if files != source_files:
+                        sync(source_files, path, files)
+                print("Success! Directories synced")
+            else:
+                print("Directories already synced")
     else:
         print("No files in path")
 else:
