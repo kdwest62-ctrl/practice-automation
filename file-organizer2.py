4bcda6021c9e95b9ede5ffbe72d73f6a23a3067c
Kear Ibale <kdwest62@gmail.com>
2026-04-15 14:07:57 +0800

    Refactor file organization logic and improve user prompts
    Refactored file size retrieval and directory creation logic to handle cases with no files. Added checks for empty directories and improved user prompts.

--- file-organizer2.py
+++ file-organizer2.py
@@ -8,29 +8,33 @@ try:
             result = num / (1024 ** 2)
             return round(result, 2)
 
-        files = []
-        sizes = []
-        for item in os.listdir(path):
-            file = os.path.join(path, item)
-            if os.path.isfile(file):
-                files.append(file)
-                sizes.append(convert(os.path.getsize(file)))
-        files_with_sizes = dict(zip(sizes, files))
+        items = os.listdir(path)
+        if len(items) > 0:
+            files = []
+            sizes = []
+            for item in items:
+                file = os.path.join(path, item)
+                if os.path.isfile(file):
+                    files.append(file)
+                    sizes.append(convert(os.path.getsize(file)))
+            files_with_sizes = dict(zip(sizes, files))
 
-        total = int(input("Number of directories to create: "))
-        if total > 0:
-            count = 1
-            while count <= total:
-                name = input(f"Directory {count} name: ")
-                dir_path = os.path.join(path, name)
-                os.mkdir(dir_path)
-                min_size = float(input("Min file size (mb): "))
-                max_size = float(input("Max file size (mb): "))
-                for size, file in files_with_sizes.items():
-                    if min_size <= size <= max_size:
-                        shutil.move(file, dir_path)
-                        print(f"{file} moved to {name}")
-                count += 1
+            total = int(input("Number of directories to create: "))
+            if total > 0:
+                count = 1
+                while count <= total:
+                    name = input(f"Directory {count} name: ")
+                    dir_path = os.path.join(path, name)
+                    os.mkdir(dir_path)
+                    min_size = float(input("Min file size (mb): "))
+                    max_size = float(input("Max file size (mb): "))
+                    for size, file in files_with_sizes.items():
+                        if min_size <= size <= max_size:
+                            shutil.move(file, dir_path)
+                            print(f"{file} moved to {name}")
+                    count += 1
+        else:
+            print("No files in path")
     else:
         print("Path not found")
 except FileExistsError:
