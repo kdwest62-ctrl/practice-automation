089940b2876e19bf499ffca0917b84caac37fe94
Kear Ibale <kdwest62@gmail.com>
2026-04-19 14:55:01 +0800

    Refactor archiving utility to handle exceptions

--- archiving-utility.py
+++ archiving-utility.py
@@ -1,51 +1,54 @@
 import shutil
 from pathlib import Path
 
-path = Path(input("Path: "))
-if path.exists():
-    dir_paths = []
-    dir_names = []
-    for item in path.iterdir():
-        if item.is_dir():
-            dir_paths.append(item)
-            dir_names.append(Path(item).name)
+try:
+    path = Path(input("Path: "))
+    if path.exists():
+        dir_paths = []
+        dir_names = []
+        for item in path.iterdir():
+            if item.is_dir():
+                dir_paths.append(item)
+                dir_names.append(Path(item).name)
 
-    if len(dir_paths) > 0:
-        numbers = [item for item in range(len(dir_paths))]
-        dirs = dict(zip(numbers, dir_names))
-        for num, dir_name in dirs.items():
-            print(num, dir_name)
+        if len(dir_paths) > 0:
+            numbers = [item for item in range(len(dir_paths))]
+            dirs = dict(zip(numbers, dir_names))
+            for num, dir_name in dirs.items():
+                print(num, dir_name)
 
-        total = int(input("Number of directories to archive: "))
-        if total > 0:
-            dir_nums = []
-            count = 0
-            while count < total:
-                dir_num = int(input("Directory to archive (number): "))
-                if dir_num in dirs.keys():
-                    dir_nums.append(dir_num)
-                    count += 1
-                else:
-                    print("Directory not found")
+            total = int(input("Number of directories to archive: "))
+            if total > 0:
+                dir_nums = []
+                count = 0
+                while count < total:
+                    dir_num = int(input("Directory to archive (number): "))
+                    if dir_num in dirs.keys():
+                        dir_nums.append(dir_num)
+                        count += 1
+                    else:
+                        print("Directory not found")
 
-            archive_names = []
-            for item in dir_nums:
-                archive_name = input(f"Archive name for {dirs[item]}: ")
-                archive_names.append(archive_name)
+                archive_names = []
+                for item in dir_nums:
+                    archive_name = input(f"Archive name for {dirs[item]}: ")
+                    archive_names.append(archive_name)
 
-            to_archive = dict(zip(dir_nums, archive_names))
-            reference = dict(zip(numbers, dir_paths))
+                to_archive = dict(zip(dir_nums, archive_names))
+                reference = dict(zip(numbers, dir_paths))
 
-            for num, name in to_archive.items():
-                dst_path = Path(input(f"Destination path for {name}: "))
-                if dst_path.exists():
-                    src = str(reference[num])
-                    dst = dst_path / name
-                    shutil.make_archive(dst, 'zip', src)
-                    print(f"{dirs[num]} archived as {name}")
-                else:
-                    print("Destination path not found")
+                for num, name in to_archive.items():
+                    dst_path = Path(input(f"Destination path for {name}: "))
+                    if dst_path.exists():
+                        src = str(reference[num])
+                        dst = dst_path / name
+                        shutil.make_archive(dst, 'zip', src)
+                        print(f"{dirs[num]} archived as {name}")
+                    else:
+                        print("Destination path not found")
+        else:
+            print("No directories in path")
     else:
-        print("No directories in path")
-else:
-    print("Path not found")
+        print("Path not found")
+except ValueError:
+    print("Invalid input")
